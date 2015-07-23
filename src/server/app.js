3a52cd4776c14e88a3927fb0ab5ad5e9f92603de
Sam Reaves <thesamreaves@gmail.com>
2015-07-22 23:13:58 -0400

    Added npm start script to run gulp server. put index assets inside of static directory. modify main app.js to use static content and path module

--- src/server/app.js
+++ src/server/app.js
@@ -1,13 +1,36 @@
-var express = require('express');
-var app = module.exports.app = exports.app = express();
+/* 
 
+	app.js
+
+	Initializes server. Sets up static content. Establishes routes and 
+	their handlers.
+
+	Written by Sam Reaves
+	July 22, 2015
+
+ */
+
+
+// Import Express and initialize server. 
+var express = require('express'),
+	app = module.exports.app = exports.app = express(),
+	path = require('path');
+
+// Establish static directory at client/public
+app.use(express.static(path.join(__dirname, '../client/static')));
+
+// Route handler for root - sends current static landing page
 app.get('/', function(req, res) {
-  res.send('Hello, World');
+  res.sendfile("index.html");
 });
 
+// Server starts listening on port 3000.
 var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
+  
+  // Grab host and port from server's address method
+  var host = server.address().address,
+  	  port = server.address().port;
 
+  // Logs a message to let dev know we're up and running.
   console.log('Example app listening at http://%s:%s', host, port);
 });
