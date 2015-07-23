3b597c0a6ec760787e7deaa6ef21a471ac3839eb
Sam Reaves <thesamreaves@gmail.com>
2015-07-22 21:55:43 -0400

    Got Express server running with Gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    server = require('gulp-express');
+
 
 gulp.task('default', function() {
   console.log('ran default task!');
+});
+
+gulp.task('server', function () {
+  
+  // Start the server at the beginning of the task
+  server.run(['src/server/app.js']);
+
+  // Put watch code here
+
 });
--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app = express();
+var app = module.exports.app = exports.app = express();
 
 app.get('/', function(req, res) {
   res.send('Hello, World');
